99425711dc67158e3451ad4c9c1c424831b82bbb
capinzor <capinzonr@unal.edu.co>
2018-04-08 22:32:54 -0500

    creando vista matches

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from './home';
 import Login from './login';
+import Matchedate from './matchedate';
 import Matches from './matches';
 
 import 'semantic-ui-css/semantic.min.css';
@@ -17,6 +18,7 @@ export default ()=>(
     <Switch>
       <Route path="/" exact component={Home}/>
       <Route path="/login" exact component={Login}/>
+      <Route path="/matchedate" exact component={Matchedate}/>
       <Route path="/matches" exact component={Matches}/>
     </Switch>
   </Router>
--- src/routes/matches.js
+++ src/routes/matches.js
@@ -6,7 +6,7 @@ import gpl from 'graphql-tag';
 
 const styles={
   grid:{
-    height:'100',
+    height:'100%',
     width:'900px',
     margin:'0 auto',
   },
@@ -19,202 +19,24 @@ const styles={
   }
 }
 
-
-
-// const matchesByDay = {};
-// let dayMatch, currentDate;
-//
-// for (const match of allMatches) {
-//   currentDate = new Date(match.date);
-//   dayMatch = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()).toString();
-//   if (!matchesByDay.hasOwnProperty(dayMatch)) //
-//     matchesByDay[dayMatch] = [];
-//   matchesByDay[dayMatch].push(match);
-// }
-//
-// for (const day in matchesByDay) {
-//   console.log(day);
-//   console.log('------');
-//   for (const match of matchesByDay[day])
-//     console.log(match);
-//   console.log();
-// }
-
-
-
 class Matches extends React.Component {
-  constructor(){
-    super()
-    this.state={allMatches:null,allTeams:null}
 
-  }
-  componentWillMount(){
-    const getAllMatches = gpl`{
-      allMatches {
-        id
-        team_local_id
-        team_visitor_id
-        date
-      }
-    }`;
-
-    const getAllTeams=gpl`{
-      allTeams {
-        id
-        name
-      }
-    }`;
-
-    graphql(getAllMatches)(
-      ({data: {allMatches=[],lodading}})=>
-        this.setState({allMatches:allMatches})
-
-
-    )
-    graphql(getAllTeams)(
-      ({data: {allTeams=[],lodading}})=>
-        this.setState({allTeams:allTeams})
-
-
-    )
-
-    console.log(this.state)
-  }
   render() {
     return (
-    <Grid  verticalAlign='middle' columns={3} centered style={styles.grid}>
-      <img src='images/APUESTAMUNDIAL2.png'/>
+    <Grid  verticalAlign='middle' centered style={styles.grid}>
+      <container>
+        <img src='images/APUESTAMUNDIAL2.png'/>
+      </container>
       <container>
         <h1>Seleccione el encuentro en el cual desea efectuar su apuesta</h1>
       </container>
-      <Grid.Row>
-        <Grid.Column>
-
-          <div>
-
-            <div style={styles.box}>
-              <h4>Junio 14</h4>
-              {/* {this.state.allMatches.map((match, i)=><li key={i}>{match.id}</li>)} */}
+      <container>
+        <div>
+          <div style={styles.box}>
+            <h2>Aqui van los partidos </h2>
             </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 15</h4>
-
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 16</h4>
-
-            </div>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 17</h4>
-              {/* {this.state.allMatches.map((match, i)=><li key={i}>{match.id}</li>)} */}
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 18</h4>
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 19</h4>
-            </div>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 20</h4>
-              {/* {this.state.allMatches.map((match, i)=><li key={i}>{match.id}</li>)} */}
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 21</h4>
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 22</h4>
-            </div>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 23</h4>
-              {/* {this.state.allMatches.map((match, i)=><li key={i}>{match.id}</li>)} */}
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 24</h4>
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 25</h4>
-            </div>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 26</h4>
-              {/* {this.state.allMatches.map((match, i)=><li key={i}>{match.id}</li>)} */}
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 27</h4>
-            </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>
-            <div style={styles.box}>
-              <h4>Junio 28</h4>
-            </div>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
+        </div>
+      </container>
     </Grid>
     )
   }
